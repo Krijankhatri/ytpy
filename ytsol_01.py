7c6556f92ba81c9d5907652e7d0e1f649cfe21a4
Krijan <dilkshetri2020@gmail.com>
2024-05-16 08:38:39 +0545

    first commit

---
+++ ytsol_01.py
@@ -0,0 +1,16 @@
+# program to add two numbers with pre defined variables
+
+# num1 = 10
+
+# num2 = 20
+
+# print("The sum of the numbers is:",num1+num2)
+
+# program to add two numbers with pre user inputs
+
+num3 = float(input("Enter the first number: "))
+
+num4 = float(input("Enter the second number: "))
+
+print("The sum of the numbers is:",num3+num4)
+
